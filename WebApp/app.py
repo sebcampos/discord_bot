936ef574322358a9deec77dda0a22474d455c4c1
sebcampos <secampos95@gmail.com>
2021-07-21 04:47:56 +0000

    working on flask deployment

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,3 +1,26 @@
+#!/home/linuxbrew/.linuxbrew/bin/python3
 from flask import Flask, redirect, url_for, request, render_template, send_from_directory
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=["GET"])
+def root():
+    return "running"
+
+#awaiting token
+@app.route('/post_me', methods=["GET","POST"])
+def setup():
+    if request.method == "POST" or request.method == "GET":
+        print(request)
+    return "<h1>Testing</h1>"
+
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=80)
+
+    
 
 

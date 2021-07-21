88d21695322643b9f565e648138d916eff20d130
sebcampos <sebcampos@criptext.com>
2021-07-20 22:20:07 -0700

    debugging

--- happyBot/spotifyClient.py
+++ happyBot/spotifyClient.py
@@ -1,9 +1,11 @@
 import requests
 from config import CLIENT_ID, CLIENT_SECRET
+redirect_uri = "34.102.16.154"
 
 class ShopifyClient:
-    def __init__(self, CLIENT_ID, CLIENT_SECRET):
+    def __init__(self, CLIENT_ID=CLIENT_ID, CLIENT_SECRET=CLIENT_SECRET):
         self.client_id = CLIENT_ID
         self.client_secret = CLIENT_SECRET
     def authorize(self):
-        requests.get(f"https://authorize.spotify.com/authorize?client={self.client_id}&response_type=code&redirect_uri=CREATE_ENDPOINT_FOR_THIS&scope=")
+        response = requests.get(f"https://authorize.spotify.com/authorize?client={self.client_id}&response_type=code&redirect_uri={redirect_uri}&scope=ugc-image-upload%20user-read-recently-played%20user-top-read%20user-read-playback-position%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20app-remote-control%20streaming%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative%20user-follow-modify%20user-follow-read%20user-library-modify%20user-library-read&state=34fFs29kd09")
+        print(response)

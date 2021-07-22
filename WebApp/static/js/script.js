830750fc1dcba1b70032ae7d7af25f80767a8e8c
sebcampos <sebcampos@criptext.com>
2021-07-22 14:59:38 -0700

    working on webapp

---
+++ WebApp/static/js/script.js
@@ -0,0 +1,9 @@
+console.log("Working");
+
+var list_elements = document.getElementById("data-list");
+
+function displayData() {
+    for (var i = 0; len = list_elements.length; i++) {
+        console.log(list_elements[i])
+    }
+}

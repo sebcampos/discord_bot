3ae2e9b89a456d58a465c513106d50c6ceb96a3e
sebcampos <sebcampos@criptext.com>
2021-07-22 08:27:19 -0700

    updating webapp

--- WebApp/app.py
+++ WebApp/app.py
@@ -5,17 +5,20 @@ from flask import Flask, redirect, url_for, request, render_template, send_from_
 app = Flask(__name__)
 
 
-
+#redirect to spotify
 @app.route("/", methods=["GET"])
 def root():
-    return "running"
+    sc = SpotifyClient()
+    redirect_link = sc.authorize()
+    return redirect(redirect_link)
 
 #awaiting token
 @app.route('/post_me', methods=["GET","POST"])
 def setup():
     if request.method == "POST" or request.method == "GET":
         print(request.url)
-    return response.content
+    
+    return "success"
 
 
 if __name__ == "__main__":

108773bb404e53b842b75cbfb2b96ac85261f600
sebcampos <sebcampos@criptext.com>
2021-07-22 14:05:12 -0700

    working on webapp

--- WebApp/SpotifyClient.py
+++ WebApp/SpotifyClient.py
@@ -59,7 +59,7 @@ class SpotifyClient:
         response = requests.get(
             f"https://api.spotify.com/v1/search?q={query}&type={type_string}&market={market}&include_external={include_external}",
             headers = {
-                "Authorization":token
+                "Authorization":f"Bearer {token}"
             }
         )
         payload = response.json()
